e212458ea99c12881c6abdd4a9dba01960a19aad
mnikitin77 <nmax_2002@mail.ru>
2019-09-08 22:33:37 +0300

    Задание 1. Не смог реализовать параметризированный тест.

---
+++ src/main/java/com/mvnikitin/lesson6/Lesson6.java
@@ -0,0 +1,32 @@
+package com.mvnikitin.lesson6;
+
+import java.util.Arrays;
+
+public class Lesson6 {
+
+    public int[] processArray(int[] source, int neededDigit) throws RuntimeException {
+
+        String errorMessage = "Входной массив должен содержать хотя бы один элемент со значением 4.";
+
+        if(source == null)
+            throw new RuntimeException(errorMessage);
+
+        int indexOfLastFound = -1;
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == neededDigit) {
+                indexOfLastFound = i;
+            }
+        }
+
+        if (indexOfLastFound == -1)
+            throw new RuntimeException(errorMessage);
+
+        return indexOfLastFound == source.length - 1 ? null :
+                Arrays.copyOfRange(
+                        source, indexOfLastFound + 1, source.length);
+    }
+
+    public int dummy () {
+        return 5;
+    }
+}
